724311998ebc8c38eff18595bbccbfc86325bed1
Kirtz Camron <kirtz.camron@gmail.com>
2026-02-23 03:33:07 -0500

    commit 9b1c2e5a8f3e4d5f6a7b8c9d0e1f2a3b4c5d6e7

--- world_cities.sql
+++ world_cities.sql
@@ -1,3 +1,39 @@
 -- Write your code below this line
--- You can run this file with the command sqlite3 world_cities.db < world_cities.sql
+-- Create the cities table
+CREATE TABLE cities (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    population INTEGER,
+    country TEXT
+);
+
+INSERT INTO cities (name, population, country) VALUES
+('New York', 8398748, 'United States'),
+('Tokyo', 13515271, 'Japan'),
+('Cairo', 9500000, 'Egypt'),
+('Sydney', 5312163, 'Australia'),
+('São Paulo', 12252023, 'Brazil'),
+('Paris', 2140526, 'France'),
+('Lagos', 14368332, 'Nigeria'),
+('Mumbai', 12442373, 'India'),
+('Osaka', 2752123, 'Japan'),
+('Beijing', 21542000, 'China');
+
+-- Select all cities
+SELECT * FROM cities;
+
+-- Select all cities in Japan
+SELECT * FROM cities
+WHERE country = 'Japan';
+
+-- Select all cities with a population greater than 10 million
+SELECT * FROM cities
+WHERE population > 10000000;
+
+-- Delete New York, Cairo, and Paris
+DELETE FROM cities
+WHERE name IN ('New York', 'Cairo', 'Paris');
+
+
+--this file with the command sqlite3 world_cities.db < world_cities.sql
 -- If you don't run this file before running the tests, some tests will fail even though your queries may be correct!
